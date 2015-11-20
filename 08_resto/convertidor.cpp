d1b18218312d2ba063b1157f98127a94c7341e37
Jacson Murillo <jmjason453@gmail.com>
2015-11-20 14:08:07 +0100

    aprediendo do y while

---
+++ 08_resto/convertidor.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+
+
+    int origen ;
+    int destino = 0;
+
+
+    printf("Escribe un numero: ");
+
+    scanf(" %i", &origen);
+
+    do {
+        destino *= 10 ;
+        destino += origen % 10;
+        origen /=10;
+    } while (origen != 0 );
+
+
+    printf ("%i\n", destino );
+
+
+    return EXIT_SUCCESS;
+}
