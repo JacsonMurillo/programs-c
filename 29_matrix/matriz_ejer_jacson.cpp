bc1dbfb1a1df46c8901f56cba64a4dd3ca0583bd
Jacson Murillo <jmjason453@gmail.com>
2016-02-26 14:30:15 +0100

    matrix

---
+++ 29_matrix/matriz_ejer_jacson.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define M 4
+#define P 3
+#define N 5
+
+int main(int argc, const char **argv){
+
+    int A[M][P];
+    int B[P][N];
+    int C[M][N];
+
+    //pedir los valores de la matriz
+    
+    //mostrarlos
+
+    //hacer la multiplacación
+
+    //mostrar el resultado
+
+
+
+
+    return EXIT_SUCCESS;
+
+}
